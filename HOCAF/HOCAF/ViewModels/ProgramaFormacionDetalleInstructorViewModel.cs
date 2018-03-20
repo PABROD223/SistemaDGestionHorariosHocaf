f30712660c017f451f87ee0e90151b73eaebc3dc
user <user@DESKTOP-8NAIA6B>
2018-03-19 20:16:59 -0500

    Se creo viewmodel para los programas de formacion

---
+++ HOCAF/HOCAF/ViewModels/ProgramaFormacionDetalleInstructorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HOCAF.ViewModels
+{
+    public class ProgramaFormacionDetalleInstructorViewModel
+    {
+        public int idPrograma { get; set; }
+        public string nombre { get; set; }
+        public bool estado { get; set; }
+    }
+}
